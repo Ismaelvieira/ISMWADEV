acfd504f8fc0f2fd17598297c8edda60a5a87d3a
Ismaelvieira <38772211+Ismaelvieira@users.noreply.github.com>
2019-11-30 23:39:45 -0300

    Update echo.js

--- echo.js
+++ echo.js
@@ -1,1 +1,1 @@
-{"records":[{"version":"DELTA-YOWA v2.2.0","code":3,"url":"https://gvwhatasapp.blogspot.com/p/gv-update.html","changelog":"⦿ Auto Reply - ⦿ Search Color - ⦿ Row Height - ⦿ Fix Home Wallpaper - ⦿ Fix Online, Last Seen Color "}]}
+{"records":[{"version":"GVWA v19.12","code":1,"url":"https://gvwhatasapp.blogspot.com/p/gv-update.html","changelog":"⦿ Auto Reply - ⦿ Search Color - ⦿ Row Height - ⦿ Fix Home Wallpaper - ⦿ Fix Online, Last Seen Color "}]}
